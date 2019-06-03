31ee68f390b013923635c4fde6fb3a059bf90ccc
Ronny Wichers Schreur <ronny@cs.ru.nl>
2019-06-03 14:39:51 +0200

    Transliterated br_range_dec32le.

--- c2rust/src/common/aes.rs
+++ c2rust/src/common/aes.rs
@@ -1,7 +1,10 @@
 use std::slice;
+extern crate libc;
+use libc::size_t;
 
 extern "C" {
     fn br_dec32le_C(src: *const u8) -> u32;
+    fn br_range_dec32le_C(v: *mut u32, num: size_t, src: *const u8);
 }
 
 #[no_mangle]
@@ -14,4 +17,20 @@ pub unsafe extern "C" fn br_dec32le(src: *const u8) -> u32 {
     assert!(result_c == result_rust);
 
     result_rust
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn br_range_dec32le(v: *mut u32, num: size_t, src: *const u8) {
+    br_range_dec32le_C(v, num, src);
+
+    let v: &mut [u32] = slice::from_raw_parts_mut(v, num);
+    let src: &[u8] = slice::from_raw_parts(src, num*4 as usize);
+
+    let mut vv:  &mut Vec<u32> = &mut v.to_vec();
+    let v2 = &mut vv;
+    common::aes::br_range_dec32le(v2, src);
+    
+    for (i, p) in v.iter_mut().enumerate() {
+        assert_eq!(*p,v2[i]);
+    }        
 }
--- common/src/aes.rs
+++ common/src/aes.rs
@@ -4,3 +4,11 @@ pub fn br_decl32le(src: &[u8]) -> u32 {
     | ((src[2] as u32) << 16)
     | ((src[3] as u32) << 24)
 }
+
+
+// TODO: check efficiency
+pub fn br_range_dec32le(v: &mut[u32], src: &[u8]) {
+    for (i, p) in v.iter_mut().enumerate() {
+        *p = br_decl32le(&src[i*4..(i+1)*4]);
+    }
+}
