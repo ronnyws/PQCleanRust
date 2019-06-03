d6270bb886ecbba869bfb5010f762049934576d8
Ronny Wichers Schreur <ronny@cs.ru.nl>
2019-06-03 15:18:00 +0200

    Remove comment with C code.

--- common/src/aes.rs
+++ common/src/aes.rs
@@ -14,13 +14,6 @@ pub fn br_range_dec32le(v: &mut[u32], src: &[u8]) {
     }
 }
 
-/*
-static inline uint32_t br_swap32(uint32_t x) {
-    x = ((x & (uint32_t)0x00FF00FF) << 8)
-        | ((x >> 8) & (uint32_t)0x00FF00FF);
-    return (x << 16) | (x >> 16);
-}
-*/
 pub fn br_swap32(x: u32) -> u32 {
     let x = ((x & 0x00FF00FF) << 8) // TODO: check integer promotion rules
         | ((x >> 8) & 0x00FF00FF);
